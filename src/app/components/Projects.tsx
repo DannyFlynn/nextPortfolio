83046996d171d719f28e927bb21ae0e3dbecfa35
Danny Flynn <flynny386@gmail.com>
2023-08-29 15:19:12 +0100

    updated with new project to add

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -70,12 +70,12 @@ const Projects = ({ projectRef }: ProjectProps) => {
                     {projects[0]['showMore'] === false ? (
                         <div>
                             <p className='mt-3'>{projects[0]['paragraph'].substring(0, 100)}
-                            </p><button className='green-text project-btns p-2' onTouchEnd={() => showFullText(1)}>Show More</button>
+                            </p><button className='green-text project-btns p-2' onTouchEnd={() => showFullText(0)}>Show More</button>
                         </div>) : (
                         <div>
                             <p className='mt-3'>{projects[0]['paragraph']}
                             </p>
-                            <button className='green-text project-btns p-2' onTouchEnd={() => showFullText(1)}>Show Less</button>
+                            <button className='green-text project-btns p-2' onTouchEnd={() => showFullText(0)}>Show Less</button>
 
                         </div>)}
                 </div>
